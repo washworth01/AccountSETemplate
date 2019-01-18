6d5148f094be734fe01278b8df0ccf917fcdb3da
Matt25969 <matt.jo.hunt@gmail.com>
2019-01-18 10:44:10 +0000

    first commit

---
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -0,0 +1,74 @@
+package com.qa.MapTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.qa.persistence.domain.Account;
+import com.qa.persistence.repository.AccountMapRepository;
+
+public class AccountServiceTest {
+
+	@Before
+	public void setup() {
+	
+	}
+	
+	@Test
+	public void addAccountTest() {
+		
+	}
+	
+	@Test
+	public void add2AccountTest() {
+		
+	}
+
+	@Test
+	public void removeAccountTest() {
+		
+	}
+	
+	@Test
+	public void remove2AccountTest() {
+		
+	}
+	
+	@Test
+	public void remove2AccountTestAnd1ThatDoesntExist() {
+		
+	}
+	
+	@Test
+	public void accountConversionToJSONTestWithEmptyMap() {
+	
+	}
+	
+	@Test
+	public void accountConversionToJSONTestEmptyMapWithConversion() {
+	
+	}
+
+	@Test
+	public void accountConversionToJSONTest() {
+		
+	}
+
+	@Test
+	public void getCountForFirstNamesInAccountWhenZeroOccurances() {
+		
+	}
+	
+	@Test
+	public void getCountForFirstNamesInAccountWhenOne() {
+		
+	}
+
+	@Test
+	public void getCountForFirstNamesInAccountWhenMult() {
+		
+	}
+
+}
