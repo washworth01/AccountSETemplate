c0053b0d73d9ca4573a8bc641688e051879bd57b
washworth01 <washworth01@gmail.com>
2019-01-29 17:14:04 +0000

    All AccountMapRepository methods fully tested.

--- src/test/java/com/qa/MapTests/AccountServiceTest.java
+++ src/test/java/com/qa/MapTests/AccountServiceTest.java
@@ -33,6 +33,13 @@ public class AccountServiceTest
 		repository.accountMap.put((long) 3, account3);
 	}
 	
+	@Test
+	public void getAllAccountsTest()
+	{
+		String json = util.getJSONForObject(repository.accountMap.values());
+		String returned = repository.getAllAccounts();
+		assertEquals("The JSON Strings are not the same.", json, returned);
+	}
 	
 	@Test
 	public void addAccountTest() 
@@ -53,7 +60,15 @@ public class AccountServiceTest
 		repository.createAccount(util.getJSONForObject(newAccount2));
 		assertEquals("The Map size has not increased", mapSize + 2, repository.accountMap.size());
 	}
- 
+	
+	@Test
+	public void getAnAccountTest()
+	{
+		String json = util.getJSONForObject(repository.accountMap.get((long)3));
+		String returned = repository.getAnAccount((long) 3);
+		assertEquals("The JSON Strings do not match", json, returned);
+	}
+  
 	@Test
 	public void removeAccountTest() 
 	{
@@ -82,6 +97,16 @@ public class AccountServiceTest
 	}
 	
 	@Test
+	public void updateAccountTest()
+	{
+		Account update = new Account("John", "Stevens", (long)1);
+		
+		String returned = repository.updateAccount(update.getAccountNumber(), util.getJSONForObject(update));
+		String json = util.getJSONForObject(repository.accountMap.get((long)1));
+		assertEquals("The JSON Strings did not match", json, returned);
+	}
+	
+	@Test
 	public void accountConversionToJSONTestWithEmptyMap() 
 	{
 		String json = util.getJSONForObject(repository.accountMap.get((long)22));
@@ -98,7 +123,7 @@ public class AccountServiceTest
 	public void accountConversionToJSONTest()
 	{
 		String json = util.getJSONForObject(repository.accountMap.get((long)1));
-		assertEquals("Json Strings did not match", "{\"firstName\":\"John\",\"lastName\":\"Cleese\",\"accountNumber\":1}", json);
+		assertEquals("JSON Strings did not match", "{\"firstName\":\"John\",\"lastName\":\"Cleese\",\"accountNumber\":1}", json);
 	}
 
 	@Test 
